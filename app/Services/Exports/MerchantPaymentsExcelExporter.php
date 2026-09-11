9770a41b50a49e41cc610881fe540f5c76601a1e
Hannah Trust <hannah18.panaligan@gmail.com>
2026-09-11 16:04:53 +0800

    fix curropted excel file

--- app/Services/Exports/MerchantPaymentsExcelExporter.php
+++ app/Services/Exports/MerchantPaymentsExcelExporter.php
@@ -36,9 +36,15 @@ final class MerchantPaymentsExcelExporter
             throw new RuntimeException('Unable to create a temporary Excel workbook.');
         }
 
+        $workbookPath = $temporaryPath.'.xlsx';
+
         try {
+            if (is_file($temporaryPath)) {
+                unlink($temporaryPath);
+            }
+
             $archive = new ZipArchive;
-            $openResult = $archive->open($temporaryPath, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+            $openResult = $archive->open($workbookPath, ZipArchive::CREATE | ZipArchive::OVERWRITE);
             if ($openResult !== true) {
                 throw new RuntimeException('Unable to open the Excel workbook archive.');
             }
@@ -48,20 +54,24 @@ final class MerchantPaymentsExcelExporter
                     if ($archive->addFromString($fileName, $contents) === false) {
                         throw new RuntimeException('Unable to add a file to the Excel workbook.');
                     }
+
+                    $archive->setCompressionName($fileName, ZipArchive::CM_DEFLATE);
                 }
             } finally {
                 $archive->close();
             }
 
-            $workbook = file_get_contents($temporaryPath);
+            $workbook = file_get_contents($workbookPath);
             if ($workbook === false) {
                 throw new RuntimeException('Unable to read the generated Excel workbook.');
             }
 
-            return $workbook;
+            return $this->makeZipCompatibleWithExcel($workbook);
         } finally {
-            if (is_file($temporaryPath)) {
-                unlink($temporaryPath);
+            foreach ([$temporaryPath, $workbookPath] as $path) {
+                if (is_file($path)) {
+                    unlink($path);
+                }
             }
         }
     }
@@ -72,9 +82,13 @@ final class MerchantPaymentsExcelExporter
      */
     private function workbookFiles(Collection $payments): array
     {
+        $timestamp = gmdate('Y-m-d\TH:i:s\Z');
+
         return [
             '[Content_Types].xml' => $this->contentTypesXml(),
             '_rels/.rels' => $this->rootRelationshipsXml(),
+            'docProps/core.xml' => $this->corePropertiesXml($timestamp),
+            'docProps/app.xml' => $this->appPropertiesXml(),
             'xl/workbook.xml' => $this->workbookXml(),
             'xl/_rels/workbook.xml.rels' => $this->workbookRelationshipsXml(),
             'xl/styles.xml' => $this->stylesXml(),
@@ -92,6 +106,8 @@ final class MerchantPaymentsExcelExporter
     <Override PartName="/xl/workbook.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml"/>
     <Override PartName="/xl/worksheets/sheet1.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"/>
     <Override PartName="/xl/styles.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml"/>
+    <Override PartName="/docProps/core.xml" ContentType="application/vnd.openxmlformats-package.core-properties+xml"/>
+    <Override PartName="/docProps/app.xml" ContentType="application/vnd.openxmlformats-officedocument.extended-properties+xml"/>
 </Types>
 XML;
     }
@@ -102,7 +118,32 @@ XML;
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
     <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="xl/workbook.xml"/>
+    <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties" Target="docProps/core.xml"/>
+    <Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties" Target="docProps/app.xml"/>
 </Relationships>
+XML;
+    }
+
+    private function corePropertiesXml(string $timestamp): string
+    {
+        return <<<XML
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <dc:creator>GatewayHub</dc:creator>
+    <cp:lastModifiedBy>GatewayHub</cp:lastModifiedBy>
+    <dcterms:created xsi:type="dcterms:W3CDTF">{$timestamp}</dcterms:created>
+    <dcterms:modified xsi:type="dcterms:W3CDTF">{$timestamp}</dcterms:modified>
+</cp:coreProperties>
+XML;
+    }
+
+    private function appPropertiesXml(): string
+    {
+        return <<<'XML'
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">
+    <Application>GatewayHub</Application>
+</Properties>
 XML;
     }
 
@@ -111,8 +152,7 @@ XML;
         return <<<'XML'
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
-    <fileVersion appName="xl" lastEdited="1" lowestEdited="1" rupBuild="1"/>
-    <workbookPr defaultThemeVersion="124226"/>
+    <workbookPr/>
     <bookViews>
         <workbookView xWindow="0" yWindow="0" windowWidth="20480" windowHeight="11905"/>
     </bookViews>
@@ -139,13 +179,13 @@ XML;
         return <<<'XML'
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
-    <numFmts count="0"/>
     <fonts count="2">
-        <font><sz val="11"/><name val="Calibri"/><family val="2"/><scheme val="minor"/></font>
-        <font><b/><sz val="11"/><name val="Calibri"/><family val="2"/><scheme val="minor"/></font>
+        <font><sz val="11"/><color rgb="FF000000"/><name val="Calibri"/><family val="2"/></font>
+        <font><b/><sz val="11"/><color rgb="FF000000"/><name val="Calibri"/><family val="2"/></font>
     </fonts>
-    <fills count="2">
+    <fills count="3">
         <fill><patternFill patternType="none"/></fill>
+        <fill><patternFill patternType="gray125"/></fill>
         <fill><patternFill patternType="solid"><fgColor rgb="FFD9EAF7"/><bgColor indexed="64"/></patternFill></fill>
     </fills>
     <borders count="1">
@@ -156,13 +196,11 @@ XML;
     </cellStyleXfs>
     <cellXfs count="2">
         <xf numFmtId="0" fontId="0" fillId="0" borderId="0" xfId="0"/>
-        <xf numFmtId="0" fontId="1" fillId="1" borderId="0" applyAlignment="1" xfId="0"><alignment horizontal="center"/></xf>
+        <xf numFmtId="0" fontId="1" fillId="2" borderId="0" xfId="0" applyFont="1" applyFill="1" applyAlignment="1"><alignment horizontal="center"/></xf>
     </cellXfs>
     <cellStyles count="1">
         <cellStyle name="Normal" xfId="0" builtinId="0"/>
     </cellStyles>
-    <dxfs count="0"/>
-    <tableStyles count="0" defaultTableStyle="TableStyleMedium2" defaultPivotStyle="PivotStyleMedium9"/>
 </styleSheet>
 XML;
     }
@@ -192,15 +230,14 @@ XML;
                     continue;
                 }
 
-                $value = htmlspecialchars($cell['value'], ENT_XML1 | ENT_QUOTES, 'UTF-8');
-                $cells[] = '<c r="'.$cellReference.'" t="inlineStr"'.$style.'><is><t xml:space="preserve">'.$value.'</t></is></c>';
+                $cells[] = $this->inlineStringCell($cellReference, $style, $cell['value']);
             }
 
             $xmlRows[] = '<row r="'.$excelRow.'">'.implode('', $cells).'</row>';
         }
 
         return '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'
-            .'<worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">'
+            .'<worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">'
             .'<dimension ref="A1:J'.$lastRow.'"/>'
             .'<sheetViews><sheetView workbookViewId="0"><pane ySplit="1" topLeftCell="A2" activePane="bottomLeft" state="frozen"/><selection pane="bottomLeft" activeCell="A2" sqref="A2"/></sheetView></sheetViews>'
             .'<sheetFormatPr defaultRowHeight="15"/>'
@@ -278,6 +315,24 @@ XML;
         return $value === null ? $this->textCell(null) : $this->numberCell($value);
     }
 
+    private function inlineStringCell(string $cellReference, string $style, string $value): string
+    {
+        $space = $value !== trim($value) ? ' xml:space="preserve"' : '';
+
+        return '<c r="'.$cellReference.'" t="inlineStr"'.$style.'><is><t'.$space.'>'.$this->xmlText($value).'</t></is></c>';
+    }
+
+    private function xmlText(string $value): string
+    {
+        $value = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F]/', '', $value) ?? '';
+        $converted = iconv('UTF-8', 'UTF-8//IGNORE', $value);
+        if ($converted !== false) {
+            $value = $converted;
+        }
+
+        return htmlspecialchars($value, ENT_XML1 | ENT_QUOTES, 'UTF-8');
+    }
+
     private function formatDate(?DateTimeInterface $date): string
     {
         return $date?->format('Y-m-d H:i:s') ?? '';
@@ -293,5 +348,47 @@ XML;
         }
 
         return $columnName;
+    }
+
+    /**
+     * PHP 8.2+ ZipArchive stamps central-directory entries as UNIX 6.3.
+     * Microsoft Excel treats those archives as corrupt and refuses to open them.
+     */
+    private function makeZipCompatibleWithExcel(string $zip): string
+    {
+        $endOfCentralDirectory = strrpos($zip, "PK\x05\x06");
+        if ($endOfCentralDirectory === false) {
+            return $zip;
+        }
+
+        $centralDirectoryOffset = unpack('V', substr($zip, $endOfCentralDirectory + 16, 4));
+        $entryCount = unpack('v', substr($zip, $endOfCentralDirectory + 10, 2));
+        if ($centralDirectoryOffset === false || $entryCount === false) {
+            return $zip;
+        }
+
+        $offset = $centralDirectoryOffset[1];
+        $count = $entryCount[1];
+
+        for ($index = 0; $index < $count; $index++) {
+            if (substr($zip, $offset, 4) !== "PK\x01\x02") {
+                break;
+            }
+
+            $zip[$offset + 4] = "\x14";
+            $zip[$offset + 5] = "\x00";
+            $zip = substr_replace($zip, pack('V', 0x20), $offset + 38, 4);
+
+            $nameLength = unpack('v', substr($zip, $offset + 28, 2));
+            $extraLength = unpack('v', substr($zip, $offset + 30, 2));
+            $commentLength = unpack('v', substr($zip, $offset + 32, 2));
+            if ($nameLength === false || $extraLength === false || $commentLength === false) {
+                break;
+            }
+
+            $offset += 46 + $nameLength[1] + $extraLength[1] + $commentLength[1];
+        }
+
+        return $zip;
     }
 }
---
+++ tests/Unit/Services/Exports/MerchantPaymentsExcelExporterTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Tests\Unit\Services\Exports;
+
+use App\Models\Payment;
+use App\Services\Exports\MerchantPaymentsExcelExporter;
+use Illuminate\Database\Eloquent\Collection;
+use SimpleXMLElement;
+use Tests\TestCase;
+use ZipArchive;
+
+class MerchantPaymentsExcelExporterTest extends TestCase
+{
+    public function test_generated_workbook_uses_microsoft_excel_compatible_zip_headers(): void
+    {
+        $workbook = $this->generateWorkbook();
+
+        $this->assertStringStartsWith('PK', $workbook);
+
+        $endOfCentralDirectory = strrpos($workbook, "PK\x05\x06");
+        $this->assertNotFalse($endOfCentralDirectory);
+
+        $centralDirectoryOffset = unpack('V', substr($workbook, $endOfCentralDirectory + 16, 4));
+        $this->assertIsArray($centralDirectoryOffset);
+
+        $this->assertSame("PK\x01\x02", substr($workbook, $centralDirectoryOffset[1], 4));
+        $this->assertSame(20, ord($workbook[$centralDirectoryOffset[1] + 4]));
+        $this->assertSame(0, ord($workbook[$centralDirectoryOffset[1] + 5]));
+    }
+
+    public function test_generated_workbook_includes_excel_required_parts_and_reserved_fills(): void
+    {
+        $entries = $this->zipEntries($this->generateWorkbook());
+
+        $this->assertArrayHasKey('[Content_Types].xml', $entries);
+        $this->assertArrayHasKey('_rels/.rels', $entries);
+        $this->assertArrayHasKey('docProps/core.xml', $entries);
+        $this->assertArrayHasKey('docProps/app.xml', $entries);
+        $this->assertArrayHasKey('xl/workbook.xml', $entries);
+        $this->assertArrayHasKey('xl/styles.xml', $entries);
+        $this->assertArrayHasKey('xl/worksheets/sheet1.xml', $entries);
+
+        $this->assertStringContainsString('patternType="none"', $entries['xl/styles.xml']);
+        $this->assertStringContainsString('patternType="gray125"', $entries['xl/styles.xml']);
+        $this->assertStringNotContainsString('scheme val="minor"', $entries['xl/styles.xml']);
+        $this->assertStringNotContainsString('defaultThemeVersion', $entries['xl/workbook.xml']);
+    }
+
+    public function test_generated_workbook_xml_parts_are_well_formed(): void
+    {
+        foreach ($this->zipEntries($this->generateWorkbook()) as $name => $contents) {
+            if (! str_ends_with($name, '.xml') && ! str_ends_with($name, '.rels')) {
+                continue;
+            }
+
+            $xml = simplexml_load_string($contents);
+            $this->assertInstanceOf(SimpleXMLElement::class, $xml, $name.' is not well-formed XML.');
+        }
+    }
+
+    public function test_generated_workbook_escapes_xml_and_strips_illegal_control_characters(): void
+    {
+        $payment = new Payment([
+            'reference_id' => 'A&B <C>',
+            'provider_reference' => "ok\x01bad",
+            'gateway_code' => 'gcash',
+            'amount' => 10.5,
+            'currency' => 'PHP',
+            'status' => 'paid',
+        ]);
+        $payment->setRelation('gateway', null);
+        $payment->setRelation('platformFee', null);
+
+        $entries = $this->zipEntries(
+            (new MerchantPaymentsExcelExporter)->generate(new Collection([$payment]))
+        );
+        $worksheet = $entries['xl/worksheets/sheet1.xml'];
+
+        $this->assertStringContainsString('A&amp;B &lt;C&gt;', $worksheet);
+        $this->assertStringNotContainsString('A&B <C>', $worksheet);
+        $this->assertStringContainsString('okbad', $worksheet);
+        $this->assertStringNotContainsString("ok\x01bad", $worksheet);
+
+        $xml = simplexml_load_string($worksheet);
+        $this->assertInstanceOf(SimpleXMLElement::class, $xml);
+    }
+
+    private function generateWorkbook(): string
+    {
+        return (new MerchantPaymentsExcelExporter)->generate(new Collection);
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    private function zipEntries(string $workbook): array
+    {
+        $temporaryPath = tempnam(sys_get_temp_dir(), 'gatewayhub-excel-test-');
+        $this->assertNotFalse($temporaryPath);
+
+        try {
+            $this->assertNotFalse(file_put_contents($temporaryPath, $workbook));
+
+            $archive = new ZipArchive;
+            $this->assertTrue($archive->open($temporaryPath) === true);
+
+            $entries = [];
+            for ($index = 0; $index < $archive->numFiles; $index++) {
+                $name = $archive->getNameIndex($index);
+                $this->assertIsString($name);
+                $contents = $archive->getFromIndex($index);
+                $this->assertIsString($contents);
+                $entries[$name] = $contents;
+            }
+
+            $archive->close();
+
+            return $entries;
+        } finally {
+            if (is_file($temporaryPath)) {
+                unlink($temporaryPath);
+            }
+        }
+    }
+}
